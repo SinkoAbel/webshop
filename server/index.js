c713fd11aa0d033f739d097fc851c5623c6b035f
Sinkó Ábel <sinkoabel@gmail.com>
2023-03-30 19:40:06 +0200

    Created MongoDB database and made connection with it in Index.Js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
 const connect = async () => {
   try {
-      // TODO:
-      // await mongoose.connect(process.env.MONGO);
-      console.log("Connected to mongoDB");
+      await mongoose.connect('mongodb+srv://ekke:ekke_2023@webshopdb.yvhhexx.mongodb.net/?retryWrites=true&w=majority');
+      console.log("Connected to MongoDB");
   } catch (error) {
       throw error;
   }

04cf927be6f8e5ea350d437f16b7ada4f173fa31
Sinkó Ábel <sinkoabel@gmail.com>
2023-03-31 13:19:40 +0200

    Database connection string added to .env file, also initialized dotenv at index.js.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 const connect = async () => {
   try {
-      await mongoose.connect('mongodb+srv://ekke:ekke_2023@webshopdb.yvhhexx.mongodb.net/?retryWrites=true&w=majority');
+      await mongoose.connect(process.env.MONGO);
       console.log("Connected to MongoDB");
   } catch (error) {
       throw error;

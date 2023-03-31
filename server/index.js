77f323a516fd1e91f5c8f3c57de5a2162161b078
Sinkó Ábel <sinkoabel@gmail.com>
2023-03-31 13:28:18 +0200

    Handled MongoDB disconnect event.

--- server/index.js
+++ server/index.js
@@ -8,11 +8,15 @@ dotenv.config();
 const connect = async () => {
   try {
       await mongoose.connect(process.env.MONGO);
-      console.log("Connected to MongoDB");
+      console.log("Connected to MongoDB!");
   } catch (error) {
       throw error;
   }
 };
+
+mongoose.connection.on("disconnect", () =>{
+    console.log("Disconnected from MongoDB!");
+});
 
 app.listen(8800, () => {
     connect();

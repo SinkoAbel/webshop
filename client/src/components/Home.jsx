a7164a1f1ef5af98afb56cbc2d62c387e7d6e26b
Tarhos László <tlaszlo96@gmail.com>
2023-04-04 18:08:29 +0200

    Frontend

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mt-20">
+      <header className=''>
+        <h1 className='text-4xl'>Ez lesz az üdvözlőképernyő homepage</h1>
+      </header>
+      <main>
+        <p className='mt-10'>Nézd meg a termékeinket a Termékek menüpontban.</p>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="container mt-20">
+      <header className=''>
+        <h1 className='text-4xl'>Ez lesz a login felület</h1>
+      </header>
+      <main>
+        <p className='mt-10'>Nézd meg a termékeinket a Termékek menüpontban.</p>
+      </main>
+    </div>
+  );
+};
+
+export default Login;

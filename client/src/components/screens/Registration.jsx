f989e150b13ce68bf1b715bc854827a271d449f7
Tarhos László <tlaszlo96@gmail.com>
2023-04-07 18:24:56 +0200

    Page routes added.

---
+++ client/src/components/screens/Registration.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Registration = () => {
+  return (
+    <div className="container mt-20">
+      <header className=''>
+        <h1 className='text-4xl'>Ez lesz a regisztrációs felület</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Registration;

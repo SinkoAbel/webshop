27506832d3c8322bdcc4e32171c8e983f36327af
tanyiG <tanyi.gyozo@gmail.com>
2023-04-11 23:00:33 +0200

    AuthController.js changed according the model changes.

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -3,61 +3,44 @@ import bcrypt from 'bcrypt';
 import { createError } from '../utils/error.js';
 import User from '../models/User.js';
 
-const secretKey = 'your-secret-key';
 
-const AuthController = {
-  async login(req, res, next) {
-    const { username, password } = req.body;
+export const register = async (req, res) => {
+  const { username, password, email } = req.body;
 
-    try {
-      const user = await User.findOne({ username });
-
-      if (!user) {
-        throw createError(401, 'Invalid credentials');
-      }
-
-      const isMatch = await bcrypt.compare(password, user.password);
-
-      if (!isMatch) {
-        throw createError(401, 'Invalid credentials');
-      }
-
-      const token = jwt.sign(
-        { userId: user._id },
-        secretKey,
-        { expiresIn: '1h' }
-      );
-
-      res.status(200).json({ token });
-    } catch (err) {
-      next(err);
+  try {
+    const userExists = await User.findOne({ $or: [{ username }, { email }] });
+    if (userExists) {
+      throw createError(400, 'Username or email already exists');
     }
-  },
 
-  async register(req, res, next) {
-    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({ username, password: hashedPassword, email });
+    await user.save();
 
-    try {
-      const existingUser = await User.findOne({ $or: [{ username }, { email }] });
-
-      if (existingUser) {
-        throw createError(409, 'Username or email already exists');
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const user = new User({
-        username,
-        email,
-        password: hashedPassword,
-      });
-
-      await user.save();
-
-      res.status(201).json({ message: 'User registered successfully' });
-    } catch (err) {
-      next(err);
-    }
-  },
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(201).json({ message: 'User created', token });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || 'Something went wrong' });
+  }
 };
 
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      throw createError(404, 'User not found');
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      throw createError(401, 'Invalid credentials');
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || 'Something went wrong' });
+  }
+};

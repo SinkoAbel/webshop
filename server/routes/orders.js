f136b78c3591af5b155977a856ace74196cc7bd3
tanyiG <tanyi.gyozo@gmail.com>
2023-04-12 18:41:04 +0200

    OrderController.js and order.js router partly done. getOrderById and updateOrder functions missing.

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,11 +1,12 @@
 import express from "express";
+import {createOrder, deleteOrder} from "../controllers/OrderController.js";
 
 const router = express.Router();
 
-router.get("/");
-router.post("/");
+router.get("/", getAllOrders);
+router.post("/", createOrder);
 router.get("/:id");
 router.put("/:id");
-router.delete("/:id");
+router.delete("/:id", deleteOrder);
 
 export default router;

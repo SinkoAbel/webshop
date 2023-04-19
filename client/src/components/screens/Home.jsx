8bc64ff9d2a4c575abb2f1f06818826d66c63d73
Tarhos László <tlaszlo96@gmail.com>
2023-04-19 19:50:56 +0200

    Homepage design and welcome text added.

--- client/src/components/screens/Home.jsx
+++ client/src/components/screens/Home.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="container mt-20">
-      <header className=''>
-        <h1 className='text-4xl'>Ez lesz az üdvözlőképernyő homepage</h1>
-      </header>
-      <main>
-        <p className='mt-10'>Nézd meg a termékeinket a Termékek menüpontban.</p>
-      </main>
-    </div>
+	<div className="flex items-center justify-center relative bg-cover bg-no-repeat bg-center h-[700px] w-full bg-[url('/public/webshop-bg3.jpg')]">
+		<div className="absolute top-0 left-0 bg-[rgba(31,44,108,.65)] w-full h-full">
+			<div className="header-text text-center mt-[250px]">
+				<h2 className="text-white text-5xl font-bold leading-1.5">
+					Üdvözöllek a Webáruházunkban!
+				</h2>
+				<p className="text-white text-2xl font-semibold my-10 tracking-wide">
+					Nézd meg a termékeinket a Termékek menüpontban.
+				</p>
+			</div>
+		</div>		
+  </div>
   );
 };
 

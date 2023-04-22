f18b2b3919fcd0bd5613041048128eea6655d88c
Sinkó Ábel <sinkoabel@gmail.com>
2023-04-22 20:29:44 +0200

    Created logic for shifting to nextPhase in cart.

--- client/src/components/screens/Cart.jsx
+++ client/src/components/screens/Cart.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import OrderProcessStepper from "../elements/OrderProcessStepper";
 import CartSummary from "../elements/CartSummary";
 import {useSelector} from "react-redux";
+import Home from "./Home";
 
 const Cart = (props) => {
 
@@ -11,6 +12,9 @@ const Cart = (props) => {
     switch (phase) {
         case 1:
             currentPhaseElement = <CartSummary/>;
+            break;
+        case 2:
+            //
             break;
         default:
             currentPhaseElement = <CartSummary/>;

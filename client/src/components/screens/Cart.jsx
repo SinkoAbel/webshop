fad4c27429f7d3913211e181051dc2628c7c01b9
Sinkó Ábel <sinkoabel@gmail.com>
2023-04-22 20:19:47 +0200

    Extended the Order.js model at server side.

--- client/src/components/screens/Cart.jsx
+++ client/src/components/screens/Cart.jsx
@@ -1,13 +1,26 @@
 import React, {useState} from 'react';
 import OrderProcessStepper from "../elements/OrderProcessStepper";
 import CartSummary from "../elements/CartSummary";
+import {useSelector} from "react-redux";
 
 const Cart = (props) => {
+
+    const phase = useSelector((state) => state.cart.phase);
+    let currentPhaseElement;
+
+    switch (phase) {
+        case 1:
+            currentPhaseElement = <CartSummary/>;
+            break;
+        default:
+            currentPhaseElement = <CartSummary/>;
+    }
+
     return (
         <>
             <OrderProcessStepper/>
 
-            <CartSummary/>
+            {currentPhaseElement}
         </>
     );
 };

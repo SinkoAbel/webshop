ec5b557aea142d44adf6f5c6bcf402478829584a
Sinkó Ábel <sinkoabel@gmail.com>
2023-04-22 18:02:51 +0200

    Created cart logic in redux.

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import registerReducer from "./reducers/registerSlice";
 import loginReducer from "./reducers/loginSlice";
+import cartReducer from "./reducers/cartSlice";
 
 export default configureStore({
     reducer: {
         register: registerReducer,
         login: loginReducer,
+        cart: cartReducer
     },
 });

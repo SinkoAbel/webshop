d39917244b6468bb74788f4648e52c38453b5585
Sinkó Ábel <sinkoabel@gmail.com>
2023-04-23 13:04:26 +0200

    Initializing order confiromation screen.

--- client/src/components/elements/OrderConfirmation.jsx
+++ client/src/components/elements/OrderConfirmation.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import axios from "axios";
 
 const OrderConfirmation = (props) => {
+
+    const endpoint = '';
+
     return (
         <div>
-            
+            <p>Köszönjük, hogy minket választott!</p>
+            <p>Rendelését sikeresen rögzítettük a rendszerünkben!</p>
         </div>
     );
 };

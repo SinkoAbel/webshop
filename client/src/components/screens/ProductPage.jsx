f271caa0a846314c032daadd9443385325298445
Tarhos László <tlaszlo96@gmail.com>
2023-06-25 21:46:40 +0200

    Some fixes in product design page

--- client/src/components/screens/ProductPage.jsx
+++ client/src/components/screens/ProductPage.jsx
@@ -33,6 +33,7 @@ const ProductPage = (props) => {
                     <h6 className='text-2xl font-semibold'>Ár: {productDetails.price} Ft.</h6>
                 </div>
                 <div className='flex flex-row items-center gap-12'>
+                    <p>Ide jön a termék mennyiségének a számlálója</p>
                     <button className='bg-violet-800 text-white font-semibold py-3 px-16 rounded-xl h-full'>Hozzáadás a kosárhoz</button>
                 </div>
             </div>

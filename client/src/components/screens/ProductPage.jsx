a85857aa069c29289e18c7b9eb78c3bdaf89e4f5
Tarhos László <tlaszlo96@gmail.com>
2023-06-25 21:05:32 +0200

    Product image added to Product Page

--- client/src/components/screens/ProductPage.jsx
+++ client/src/components/screens/ProductPage.jsx
@@ -21,12 +21,18 @@ const ProductPage = (props) => {
 
         fetchEndpoint();
     }, [endpoint]);
-
-    console.log(productDetails.product_name);
-
+ 
     return (
-        <div>
-            <p>{productDetails.product_name}</p>
+        <div className='flex flex-col justify-between lg:flex-row gap-16 lg:items-center'>
+            <div className='flex flex-col gap-6 lg:w-2/5'>
+                <img src={productDetails.photo_path} alt="" className='w-full h-full aspect-square object-cover rounded-xl'/>
+            </div>
+            <div className='flex flex-col gap-4 lg:w-3/4'>
+                <div className='text-left'>
+                    <h1 className='text-3xl font-bold mb-6 text-violet-600'>Termék neve</h1>
+                    <h6 className='text-2xl font-semibold'>Termék ára</h6>
+                </div>
+            </div>
         </div>
     );
 };
